b2b9a87813a957416cd27050cd99216a14933698
Julien Bérubé <julien.berube@bnc.ca>
2021-11-24 17:48:52 -0500

    section 19 - handling GraphQL Error Responses

---
+++ client/src/requests.js
@@ -0,0 +1,52 @@
+const endpointUrl = "http://localhost:9000/graphql";
+
+async function graphqlRequest(query, variables = {}) {
+  const response = await fetch(endpointUrl, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ query, variables }),
+  });
+  const responseBody = await response.json();
+  if (responseBody.errors) {
+    const message = responseBody.errors
+      .map((error) => error.message)
+      .join("/n");
+    throw new Error(message);
+  }
+  return responseBody.data;
+}
+
+export async function loadJobs() {
+  const query = `
+    query {
+      jobs {
+        id
+        title
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+  const { jobs } = await graphqlRequest(query);
+  return jobs;
+}
+
+export async function loadJob(id) {
+  const query = `
+    query jobQuery($id: ID!) {
+      job(id: $id) {
+        id
+        title
+        company {
+          id
+          name
+        }
+        description
+      }
+    }
+  `;
+  const { job } = await graphqlRequest(query, { id });
+  return job;
+}
